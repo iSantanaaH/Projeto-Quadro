818c168901e9b5e947ac904328ab9020a9d8f716
Micael Santana <micaelsantana.dev@gmail.com>
2023-07-26 19:54:49 -0300

    Update CardTask Validation and message Error

--- front-end_react/src/components/CardTask.tsx
+++ front-end_react/src/components/CardTask.tsx
@@ -1,23 +1,22 @@
-{
-  /* Outros */
-}
 import { BiDotsHorizontal } from "react-icons/bi";
 import { BiPlus } from "react-icons/bi";
-import { useRef, useState, KeyboardEvent, useEffect } from "react";
+import { useRef, useState, KeyboardEvent } from "react";
 
-{
-  /* Estilos */
-}
 import styles from "../styles/CardTask.module.css";
 
 const CardTask = () => {
   const [createNewDivTask, setIsCreateNewDivTask] = useState(0);
-  const [errorEmpty, setErrorEmpty] = useState(false);
+  const [focusedTextarea, setFocusedTextarea] =
+    useState<HTMLTextAreaElement | null>(null);
   const textareaMainCard = useRef<HTMLTextAreaElement>(null);
   const contentTaskTextarea = useRef<HTMLTextAreaElement>(null);
+  const [isContentEmpty, setIsContentEmpty] = useState(true);
+  const [emptyTextareaIndex, setEmptyTextareaIndex] = useState<number | null>(
+    null
+  );
 
   const handleCreateNewDivTask = () => {
-      setIsCreateNewDivTask(createNewDivTask + 1);
+    setIsCreateNewDivTask(createNewDivTask + 1);
   };
 
   const handleRenderTaskDivs = () => {
@@ -25,16 +24,23 @@ const CardTask = () => {
 
     for (let i = 0; i < createNewDivTask; i++) {
       taskDivs.push(
-        <div className={styles.secondMainCardTask}>
-          <div className={styles.containerDescription}>
-            <div className={styles.divBackgroundTextarea}></div>
-            <textarea
-              onKeyDown={handleKeyDown}
-              onBlur={handleEmptyTextarea}
-              ref={contentTaskTextarea}
-              className={styles.titleTaskUser}
-              autoFocus
-            ></textarea>
+        <div key={i} className={styles.taskDivWrapper}>
+          <div className={styles.secondMainCardTask}>
+            <div className={styles.containerDescription}>
+              <div className={styles.divBackgroundTextarea}></div>
+
+              <textarea
+                onKeyDown={handleKeyDown}
+                onBlur={handleEmptyContentTask}
+                ref={contentTaskTextarea}
+                className={styles.titleTaskUser}
+                autoFocus
+                data-index={i} 
+              ></textarea>
+              {isContentEmpty && emptyTextareaIndex === i && (
+                <div className={styles.errorMessage}>Campo obrigatório</div>
+              )}
+            </div>
           </div>
         </div>
       );
@@ -47,39 +53,65 @@ const CardTask = () => {
       event.preventDefault();
       if (textareaMainCard.current && contentTaskTextarea) {
         textareaMainCard.current.blur();
+        if (focusedTextarea === contentTaskTextarea.current) {
+          setFocusedTextarea(null);
+        }
         contentTaskTextarea.current?.blur();
       }
     }
   };
 
-  const handleEmptyTextarea = (
+  const handleEmptyMainTitleCard = (
     event: React.ChangeEvent<HTMLTextAreaElement>
   ) => {
     const value = event.target.value;
 
-    if (value.trim() === "") {
-      textareaMainCard.current?.focus();
+    if (textareaMainCard) {
+      if (value !== "") {
+        textareaMainCard.current?.blur();
+      } else {
+        textareaMainCard.current?.focus();
+      }
     }
   };
 
+  const handleEmptyContentTask = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const value = event.target.value;
+    const isEmpty = value.trim() === "";
 
-
-  useEffect(() => {
-    if (textareaMainCard.current) {
-      textareaMainCard.current.focus();
+    setIsContentEmpty(isEmpty);
+    if (isEmpty) {
+      const textareaIndex = parseInt(
+        event.target.getAttribute("data-index") || "",
+        10
+      );
+      setEmptyTextareaIndex(textareaIndex);
+    } else {
+      setEmptyTextareaIndex(null);
     }
-  });
+
+    if (contentTaskTextarea) {
+      if (value !== "") {
+        contentTaskTextarea.current?.blur();
+      } else {
+        contentTaskTextarea.current?.focus();
+      }
+    }
+  };
 
   const handleButtonClick = () => {
     const valueMainCard = textareaMainCard.current?.value;
     const valueContentTask = contentTaskTextarea.current?.value;
 
-    if(valueMainCard?.trim() !== "" && valueContentTask?.trim() !== "") {
+    if (valueMainCard?.trim() !== "" && valueContentTask?.trim() !== "") {
+      setIsContentEmpty(false);
       handleCreateNewDivTask();
     } else {
-      setErrorEmpty(true);
+      setIsContentEmpty(true);
     }
-  }
+  };
 
   return (
     <div className={styles.flexCardTask}>
@@ -93,7 +125,9 @@ const CardTask = () => {
                     id="textCardTask"
                     ref={textareaMainCard}
                     onKeyDown={handleKeyDown}
+                    onBlur={handleEmptyMainTitleCard}
                     className={styles.firstTitleCard}
+                    autoFocus
                   ></textarea>
                 </div>
 
